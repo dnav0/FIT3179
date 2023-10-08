3d25fe0491cb179316e7510524d7729987490447
dnav0002 <dnav0002@student.monash.edu>
2023-10-08 22:04:42 +1100

    feat: second vis completed and grid setup

--- homework_week9/js/homework_vis.js
+++ homework_week9/js/homework_vis.js
@@ -1,3 +1,8 @@
-var spec2 = "js/homework_vis.json";
-vegaEmbed('#choropleth_map', spec2).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+var spec1 = "js/homework_vis.json";
+var spec2 = "js/homework_vis2.json";
+
+vegaEmbed('#choropleth_map', spec1, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#bar_chart', spec2, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
