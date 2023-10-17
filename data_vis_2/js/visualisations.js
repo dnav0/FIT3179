e3dcc66f852e65fd0f0acba87a8978596546a494
dnav0002 <dnav0002@student.monash.edu>
2023-10-17 17:21:25 +1100

    feat: bubble plot created and data updated

--- data_vis_2/js/visualisations.js
+++ data_vis_2/js/visualisations.js
@@ -1,6 +1,7 @@
 var spec1 = "js/map.json";
 var spec2 = "js/multiples.json";
 var spec3 = "js/bump.json";
+var spec4 = "js/bubble.json";
 
 vegaEmbed('#choropleth_map', spec1, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -9,4 +10,7 @@ vegaEmbed('#bar_chart', spec2, {"actions": false}).then(function (result) { // A
 }).catch(console.error);
 
 vegaEmbed('#bump_chart', spec3, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#bubble_plot', spec4, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
