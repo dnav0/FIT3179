2958e3dff8556e35bcfb73a966468be0854810e6
dnav0002 <dnav0002@student.monash.edu>
2023-10-17 15:12:57 +1100

    fix: flag file upload and fix

--- data_vis_2/js/visualisations.js
+++ data_vis_2/js/visualisations.js
@@ -1,6 +1,6 @@
 var spec1 = "js/map.json";
 var spec2 = "js/multiples.json";
-var spec2 = "js/bump.json";
+var spec3 = "js/bump.json";
 
 vegaEmbed('#choropleth_map', spec1, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
