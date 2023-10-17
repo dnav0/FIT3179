124309083d77653b1dc3744e1292ad6a2594675f
dnav0002 <dnav0002@student.monash.edu>
2023-10-17 22:15:42 +1100

    feat: visualisation final updates

--- data_vis_2/js/visualisations.js
+++ data_vis_2/js/visualisations.js
@@ -6,7 +6,7 @@ var spec4 = "js/bubble.json";
 vegaEmbed('#choropleth_map', spec1, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#bar_chart', spec2, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed('#multiples', spec2, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed('#bump_chart', spec3, {"actions": false}).then(function (result) { // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
